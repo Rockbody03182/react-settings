18e1c79718df871b326841193090f92da8c57b0f
Jade <rockbody03182@gmail.com>
2023-05-09 14:45:31 +0900

    [feat] Breadcrumb 기능 수정
    url 체크 후 페이지 정보 Breadcrumb에 전달 완료.
    
    Issue 없음

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,22 +12,48 @@ const Header = () => {
   useEffect(() => {
     if (!!location) {
       let path = location.split("/");
-      switch (path[1]) {
-        case "acount":
+      let mapping = path[1]+"/"+path[2];
+
+      switch (mapping) {
+        case "member/account":
           setMiddleName("회원 관리");
           setSmallName("회원 관리");
           break;
-        case "publisher_list":
+        case "publisher/lists":
           setMiddleName("퍼블리셔 관리");
-          setSmallName("영역 관리");
+          setSmallName("영역 현황");
           break;
-        case "dsp_list":
+        case "publisher/listInfo":
+          setMiddleName("퍼블리셔 관리");
+          setSmallName("노출 관리");
+          break;
+        case "dsp/lists":
           setMiddleName("DSP 관리");
           setSmallName("광고 관리");
           break;
-        case "media_list":
+        case "media/lists":
           setMiddleName("매체 관리");
-          setSmallName("매체 리스트");
+          setSmallName("영역 관리");
+          break;
+        case "stats/lists":
+          setMiddleName("통계/정산 관리");
+          setSmallName("일별 통계");
+          break;
+        case "stats/publisher":
+          setMiddleName("통계/정산 관리");
+          setSmallName("퍼블리셔 정산");
+          break;
+        case "stats/dsp-day":
+          setMiddleName("통계/정산 관리");
+          setSmallName("DSP 통계");
+          break;
+        case "stats/dsp":
+          setMiddleName("통계/정산 관리");
+          setSmallName("DSP 정산");
+          break;
+        case "etc/zonetype":
+          setMiddleName("기타 설정");
+          setSmallName("영역 타입");
           break;
         default:
           setMiddleName();
@@ -42,7 +68,7 @@ const Header = () => {
       <header className="header-wrap">
         <div className="inner d-flex align-items-center">
           <Breadcrumb>
-            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+            {/* <Breadcrumb.Item href="/">Home</Breadcrumb.Item> */}
             {!!MiddleName ? (
               <Breadcrumb.Item active>{MiddleName}</Breadcrumb.Item>
             ) : null}
